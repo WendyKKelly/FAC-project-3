0bda943c3296c2924da287c0080c1b934f169b89
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-07-05 10:36:57 -0700

    add date back in

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
+let today = new Date();
+var day = today.getDay();
+var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+var date = today.getMonth()+1+'-'+(today.getDate())+'-'+today.getFullYear();
+var time = today.getHours() + ":" + today.getMinutes();
+var dateTime = date+' and it is '+time + " o'clock";
+ 
+document.getElementById("displayDateTime").innerHTML = ' Today is ' + daylist[day] + dateTime;
+
+
 tinymce.init({
     selector: '#myTextarea',
     width: 600,
