8cac89331b60aac838a058386752742bd0f66d07
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-07-06 11:34:06 -0700

    try again with the tinymce

--- scripts.js
+++ scripts.js
@@ -15,56 +15,6 @@ tinymce.init({
     
   });
 
-  const commentBox = document.querySelector("#textarea");
-  if(commentBox) {
-    commentBox.addEventListener("submit", function(e) {
-      submitForm(e, this);
-    });
-  }
-  async function submitForm(e, form) {
-    e.preventDefault();
-    const btnSubmit = document.getElementById('btnSubmit');
-    btnSubmit.disabled = true;
-    setTimeout(() => btnSubmit.disabled = false, 2000);
-
-    const jsonFormData = buildJsonFormData(form);
-
-    const headers = buildHeaders();
-
-    const response = await fetchService.performPostHttpRequest('https://jsonplaceholder.tyicode.come/posts', headers, jsonFormData);
-
-    if(response)
-    window.location = '/success.html?Name-$[response.Name]$Email=$[response.Email]&id=$[response.id]';
-    else
-    alert('an error occured.');
-  }
-
-  function buildJsonFormData(form) {
-    const jsonFormData = { };
-    for(const pair of new FormData(form)) {
-      jsonFormData[pair[0]] = pair[1];
-    }
-    return jsonFormData;
-  }
-
-  async function performPostHttpRequest(fetchLink, body) {
-    if(!fetchLink || !body) {
-      throw new Error("One or more POST request parameters was not passed.");
-    }
-    try {
-      const rawResponse = await fetch(fetchLink, {
-        method: "POST",
-        body: JSON.stringify(body)
-      });
-      const content = await rawResponse.json();
-      return content;
-    }
-    catch(err) {
-      console.error(`Error at fetch POST: $({err}`);
-      throw err;
-    }
-  }
-
 
   const textarea = document.querySelector("textarea");
 
