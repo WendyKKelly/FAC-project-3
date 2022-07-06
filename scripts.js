8f371ca34a775d34ffdf91b2db07da97b55bb053
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-07-06 11:48:01 -0700

    tinymce again

--- scripts.js
+++ scripts.js
@@ -16,23 +16,4 @@ tinymce.init({
   });
 
 
-  const textarea = document.querySelector("textarea");
-
-  
-  // Returns text statistics for the specified editor by id
-function getStats(textarea) {
-  var body = tinymce.get(textarea).getBody(), text = tinymce.trim(body.innerText || body.textContent);
-
-  return {
-      chars: text.length,
-      words: text.split(/[\w\u2019\'-]+/).length
-  };
-}
-if (getStats('content').words < 10) {
-  alert("You need to enter 10 words or more.");
-  return;
-}
-
-// Submit the form
-document.forms[0].submit();
-
+ 
