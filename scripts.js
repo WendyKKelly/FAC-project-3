ad06377e808272cc7740c15657e87e68340268bd
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-07-06 10:43:32 -0700

    again char count

--- scripts.js
+++ scripts.js
@@ -70,11 +70,19 @@ tinymce.init({
 
   
   // Returns text statistics for the specified editor by id
-function getStats(id) {
-  var body = tinymce.get(id).getBody(), text = tinymce.trim(body.innerText || body.textContent);
+function getStats(textarea) {
+  var body = tinymce.get(textarea).getBody(), text = tinymce.trim(body.innerText || body.textContent);
 
   return {
       chars: text.length,
       words: text.split(/[\w\u2019\'-]+/).length
   };
+}
+if (getStats('content').words < 10) {
+  alert("You need to enter 10 words or more.");
+  return;
+}
+
+// Submit the form
+document.forms[0].submit();
 }
